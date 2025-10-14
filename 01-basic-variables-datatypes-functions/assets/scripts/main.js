db4d44b451553279a31a815dbcb8c7284e5f7997
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-10-14 11:28:51 +0330

    [feat] implement all calculation operators

--- 01-basic-variables-datatypes-functions/assets/scripts/main.js
+++ 01-basic-variables-datatypes-functions/assets/scripts/main.js
@@ -1,1 +1,33 @@
-alert("Hello, World!");
+const defaultNumber = 0;
+let finalResult = defaultNumber;
+
+// return the user_input as integer type
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function sum() {
+  const userInput = getUserInput();
+  finalResult += userInput;
+  outputResult(finalResult, "");
+}
+function subtract() {
+  const userInput = getUserInput();
+  finalResult -= userInput;
+  outputResult(finalResult, "");
+}
+function multiplay() {
+  const userInput = getUserInput();
+  finalResult *= userInput;
+  outputResult(finalResult, "");
+}
+function divide() {
+  const userInput = getUserInput();
+  finalResult /= userInput;
+  outputResult(finalResult, "");
+}
+
+addBtn.addEventListener("click", sum);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiplay);
+divideBtn.addEventListener("click", divide);
