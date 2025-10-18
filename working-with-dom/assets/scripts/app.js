fe913e19f0273cf6a057d83d114303c4d06ae015
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-10-18 23:21:47 +0330

    [feat] validation user inputs for add movie modal

--- working-with-dom/assets/scripts/app.js
+++ working-with-dom/assets/scripts/app.js
@@ -1,15 +1,36 @@
 const backdrop = document.getElementById("backdrop");
+const addMovieModal = document.getElementById("add-modal");
+
+// inputs
+// Movie Modal Inputs
+const addMovieModalUserInputs = addMovieModal.querySelectorAll("input");
 
 // buttons
 // add modal
-const addMovieModal = document.getElementById("add-modal");
 const addMovieBtn = document.querySelector("header button");
 const cancelAddMovieModal = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieModal = cancelAddMovieModal.nextElementSibling;
 
 // expression functions
 const toggleModal = () => {
   addMovieModal.classList.toggle("visible");
   backdrop.classList.toggle("visible");
+};
+// handler
+const addMovieHandler = () => {
+  const title = addMovieModalUserInputs[0].value.trim();
+  const imageUrl = addMovieModalUserInputs[1].value.trim();
+  const ratingString = addMovieModalUserInputs[2].value.trim();
+  const ratingNumber = +ratingString;
+
+  // check if title and imageUrl is empty
+  if (!title || !imageUrl) {
+    alert("Movie Title or Image Url are empty!");
+  }
+  // check rating
+  if (!ratingString || ratingNumber < 1 || ratingNumber > 5) {
+    alert("Raing must be between 1 and 5");
+  }
 };
 
 // listeners
@@ -17,3 +38,5 @@ addMovieBtn.addEventListener("click", toggleModal);
 backdrop.addEventListener("click", toggleModal);
 // -- modal buttons
 cancelAddMovieModal.addEventListener("click", toggleModal);
+
+confirmAddMovieModal.addEventListener("click", addMovieHandler);
