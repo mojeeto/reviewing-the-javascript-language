a80310ff81bdeade76ee5f1c45f2b66fa452ef6b
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-10-18 23:05:21 +0330

    [feat] open/close modal

--- working-with-dom/assets/scripts/app.js
+++ working-with-dom/assets/scripts/app.js
@@ -1,6 +1,19 @@
+const backdrop = document.getElementById("backdrop");
+
+// buttons
+// add modal
 const addMovieModal = document.getElementById("add-modal");
 const addMovieBtn = document.querySelector("header button");
+const cancelAddMovieModal = addMovieModal.querySelector(".btn--passive");
 
-addMovieBtn.addEventListener("click", () => {
+// expression functions
+const toggleModal = () => {
   addMovieModal.classList.toggle("visible");
-});
+  backdrop.classList.toggle("visible");
+};
+
+// listeners
+addMovieBtn.addEventListener("click", toggleModal);
+backdrop.addEventListener("click", toggleModal);
+// -- modal buttons
+cancelAddMovieModal.addEventListener("click", toggleModal);
