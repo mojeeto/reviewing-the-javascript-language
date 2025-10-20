8ba8e00899841c5d59f7ba1b0c76cba9a60daefc
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-10-20 11:47:25 +0330

    [feat] deleting movies

--- working-with-dom/assets/scripts/app.js
+++ working-with-dom/assets/scripts/app.js
@@ -1,8 +1,15 @@
 const backdrop = document.getElementById("backdrop");
 const addMovieModal = document.getElementById("add-modal");
 const section = document.getElementById("entry-text");
+const movieListUL = document.getElementById("movie-list");
 
-const movies = [];
+const movies = [
+  { id: 1, title: 1, imageUrl: 1, rating: 1 },
+  { id: 2, title: 2, imageUrl: 2, rating: 2 },
+  { id: 3, title: 3, imageUrl: 3, rating: 3 },
+  { id: 4, title: 4, imageUrl: 4, rating: 4 },
+  { id: 5, title: 5, imageUrl: 5, rating: 5 },
+];
 
 // inputs
 // Movie Modal Inputs
@@ -26,7 +33,21 @@ const updateUI = () => {
   else section.style.display = "none";
 };
 
-const renderNewMovie = (title, imageUrl, rating) => {
+const deleteMovieHandler = (id) => {
+  let deletedMovieIndex = null;
+  movies.forEach((movie, index) => {
+    if (movie.id === id) deletedMovieIndex = index;
+  });
+  if (deletedMovieIndex === null) {
+    alert("Movie not found!");
+    return;
+  }
+  movies.splice(deletedMovieIndex, 1);
+  movieListUL.children[deletedMovieIndex].remove();
+  updateUI();
+};
+
+const renderNewMovie = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement("li");
   newMovieElement.classList.add("movie-element");
   newMovieElement.innerHTML = `
@@ -38,8 +59,7 @@ const renderNewMovie = (title, imageUrl, rating) => {
       <p>${rating}/5 stars</p>
     </div>
   `;
-
-  const movieListUL = document.getElementById("movie-list");
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(this, id));
   movieListUL.appendChild(newMovieElement);
 };
 
@@ -65,6 +85,7 @@ const addMovieHandler = () => {
   }
 
   const newMovie = {
+    id: Math.random().toString(),
     title,
     imageUrl,
     rating: ratingNumber,
@@ -72,7 +93,7 @@ const addMovieHandler = () => {
 
   movies.push(newMovie);
   updateUI();
-  renderNewMovie(title, imageUrl, ratingNumber);
+  renderNewMovie(newMovie.id, title, imageUrl, ratingNumber);
   toggleModal();
 };
 
@@ -83,3 +104,11 @@ backdrop.addEventListener("click", toggleModal);
 cancelAddMovieModal.addEventListener("click", toggleModal);
 
 confirmAddMovieModal.addEventListener("click", addMovieHandler);
+
+// main
+if (movies.length > 0) {
+  updateUI();
+  movies.forEach(({ id, title, imageUrl, rating }) => {
+    renderNewMovie(id, title, imageUrl, rating);
+  });
+}
