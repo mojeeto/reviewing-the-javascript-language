197d4dd98f8c64b7d0e25e4c7841011e8d88adbd
Mojtaba Nouri <mojtaba01nouri@gmail.com>
2025-10-28 22:28:14 +0330

    [feat] add some more classes

--- learning-oop/assets/scripts/app.js
+++ learning-oop/assets/scripts/app.js
@@ -11,17 +11,41 @@ class Product {
     this.description = description;
   }
 }
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const productLI = document.createElement("li");
+    productLI.classList.add("product-item");
+    // -- with innerHTML `<div><img src=imageurl alt=title><div class=product-item__content>
+    // h2 title h3 price with $ and production descriptio in p tag and button to add to cart`
+    productLI.innerHTML = `<div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" />
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</p>
+        </div>
+      </div>`;
+
+    return productLI;
+  }
+}
 // create object about productList
-const productList = {
-  products: [
+class ProductList {
+  products = [
     new Product(
       "IPHONE 17 Pro Max",
       "https://api2.zoomit.ir/media/68c0797d2e4b43cec3e90539?w=1920&q=80",
       1599.99,
       "Some description about iphone 17 pro max",
     ),
-  ],
-  // make method for render
+  ];
+
   render() {
     // -- in redner method select app
     const app = document.getElementById("app");
@@ -30,23 +54,13 @@ const productList = {
     productUL.classList.add("product-list");
     // -- for each of products create li with class name 'product-item'
     for (const product of this.products) {
-      const productLI = document.createElement("li");
-      productLI.classList.add("product-item");
-      // -- with innerHTML `<div><img src=imageurl alt=title><div class=product-item__content>
-      // h2 title h3 price with $ and production descriptio in p tag and button to add to cart`
-      productLI.innerHTML = `<div>
-        <img src="${product.imageUrl}" alt="{product.title}" />
-        <div class="product-item__content">
-          <h2>${product.title}</h2>
-          <h3>\$${product.price}</h3>
-          <p>${product.description}</p>
-          <button>Add to Cart</p>
-        </div>
-      </div>`;
+      const productItem = new ProductItem(product);
+      const productLI = productItem.render();
       productUL.appendChild(productLI);
     }
     app.appendChild(productUL);
-  },
-};
+  }
+}
 
+const productList = new ProductList();
 productList.render();
